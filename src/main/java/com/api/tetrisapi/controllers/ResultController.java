c5c2fdc934c3f9baf251598a95ac34a48e73968e
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2023-06-13 11:25:18 -0300

    feat: add getAll, getOne and update result files

--- src/main/java/com/api/tetrisapi/controllers/ResultController.java
+++ src/main/java/com/api/tetrisapi/controllers/ResultController.java
@@ -6,11 +6,14 @@ import com.api.tetrisapi.repositories.ResultRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ResultController {
@@ -23,5 +26,30 @@ public class ResultController {
         var resultModel = new ResultModel();
         BeanUtils.copyProperties(resultRecordDto, resultModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(resultRepository.save(resultModel));
+    }
+
+    @GetMapping("/results")
+    public ResponseEntity<List<ResultModel>> getAllResults() {
+        return ResponseEntity.status(HttpStatus.OK).body(resultRepository.findAll(Sort.by(Sort.Direction.DESC, "score")));
+    }
+
+    @GetMapping("/results/{id}")
+    public ResponseEntity<Object> getOneResult(@PathVariable(value="id") UUID id){
+        Optional<ResultModel> result0 = resultRepository.findById(id);
+        if(result0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Result not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(result0.get());
+    }
+
+    @PutMapping("/results/{id}")
+    public ResponseEntity<Object> updateResult(@PathVariable(value="id") UUID id, @RequestBody @Valid ResultRecordDto resultRecordDto){
+        Optional<ResultModel> result0 = resultRepository.findById(id);
+        if(result0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Result not found");
+        }
+        var resultModel = result0.get();
+        BeanUtils.copyProperties(resultRecordDto, resultModel);
+        return ResponseEntity.status(HttpStatus.OK).body(resultRepository.save(resultModel));
     }
 }
